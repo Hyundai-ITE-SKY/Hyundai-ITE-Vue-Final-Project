aa99ddbec1ab3914bac5a3f992e8f68cd064647d
fxdmin <go.jminn@gmail.com>
2021-11-22 15:30:06 +0900

    [SKY-11] feat: 로그인 화면

--- src/router/home.js
+++ src/router/home.js
@@ -3,4 +3,8 @@ export default [
     path: "/",
     component: () => import(/* webpackChunkName: "home" */ "../views/home/Home"),
   },
+  {
+    path: "/login",
+    component: () => import(/* webpackChunkName: "home" */ "../views/home/Login"),
+  },
 ];
