b74973a20c5ebbd9f7ae580264d7947aa103ada8
smc5720 <smc5720@gmail.com>
2021-11-22 22:56:12 +0900

    [SKY-2][SKY-8] feat, refactor: 홈 화면, GNB 수정 및 사이드바 구현

--- src/views/home/NewProduct.vue
+++ src/views/home/NewProduct.vue
@@ -1,6 +1,78 @@
 <!-- 컴포넌트 UI 정의 -->
 <template>
-  <div>NEW ITEM</div>
+  <div>
+    <div class="font-weight-bold text-center mt-2">NEW ITEM</div>
+
+    <v-btn-toggle tile color="black accent-1" group>
+      <v-btn value="left"> Man </v-btn>
+      <v-btn value="center"> Woman </v-btn>
+      <v-btn value="right"> KIDS </v-btn>
+      <v-btn value="justify"> LIFE STYLE </v-btn>
+    </v-btn-toggle>
+
+    <div class="text-center mt-2">
+      <v-chip style="font-weight: bolder">OUTER</v-chip>
+    </div>
+
+    <v-slide-group class="pa-4" active-class="success">
+      <v-slide-item v-for="n in 10" :key="n">
+        <div>
+          <v-card color="grey lighten-1" class="ma-2" height="33vw" width="25vw">
+            <v-sheet color="white" elevation="0" class="text-center pa-1">{{ n }}</v-sheet>
+            <v-img
+              lazy-src="@/assets/images/event/eventloader.jpg"
+              src="@/assets/images/item/item1.jpg"
+            ></v-img>
+          </v-card>
+          <div class="text-center text-caption" style="font-weight: bolder">LOIVIE</div>
+          <div class="text-center text-caption">수딩 미스트</div>
+          <div class="text-center text-caption" style="font-weight: bolder">9,900원</div>
+        </div>
+      </v-slide-item>
+    </v-slide-group>
+
+    <div class="text-center mt-2">
+      <v-chip style="font-weight: bolder">TOP</v-chip>
+    </div>
+
+    <v-slide-group class="pa-4" active-class="success">
+      <v-slide-item v-for="n in 10" :key="n">
+        <div>
+          <v-card color="grey lighten-1" class="ma-2" height="33vw" width="25vw">
+            <v-sheet color="white" elevation="0" class="text-center pa-1">{{ n }}</v-sheet>
+            <v-img
+              lazy-src="@/assets/images/event/eventloader.jpg"
+              src="@/assets/images/item/item1.jpg"
+            ></v-img>
+          </v-card>
+          <div class="text-center text-caption" style="font-weight: bolder">LOIVIE</div>
+          <div class="text-center text-caption">수딩 미스트</div>
+          <div class="text-center text-caption" style="font-weight: bolder">9,900원</div>
+        </div>
+      </v-slide-item>
+    </v-slide-group>
+
+    <div class="text-center mt-2">
+      <v-chip style="font-weight: bolder">PANTS</v-chip>
+    </div>
+
+    <v-slide-group class="pa-4" active-class="success">
+      <v-slide-item v-for="n in 10" :key="n">
+        <div>
+          <v-card color="grey lighten-1" class="ma-2" height="33vw" width="25vw">
+            <v-sheet color="white" elevation="0" class="text-center pa-1">{{ n }}</v-sheet>
+            <v-img
+              lazy-src="@/assets/images/event/eventloader.jpg"
+              src="@/assets/images/item/item1.jpg"
+            ></v-img>
+          </v-card>
+          <div class="text-center text-caption" style="font-weight: bolder">LOIVIE</div>
+          <div class="text-center text-caption">수딩 미스트</div>
+          <div class="text-center text-caption" style="font-weight: bolder">9,900원</div>
+        </div>
+      </v-slide-item>
+    </v-slide-group>
+  </div>
 </template>
 
 <script>
