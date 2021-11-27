cc6a1318f3ba5cdba0b38aec1e74f8c045f09193
fxdmin <go.jminn@gmail.com>
2021-11-27 20:17:16 +0900

    feat: 장바구니 체크박스 선택 구현
    - 전체 선택
    - 일부 선택하다가 전체 개수 도달하면 전체선택박스도 체크

--- src/views/member/Cart.vue
+++ src/views/member/Cart.vue
@@ -3,7 +3,7 @@
 <template>
   <div>
     <div class="d-flex justify-space-between mt-5 ml-4 mr-4 mb-0">
-      <v-checkbox color="black" class="mt-0 pt-0">
+      <v-checkbox color="black" class="mt-0 pt-0" v-model="selectedAll">
         <template v-slot:label class="mb-0">
           <div style="font-size: 14px">전체선택</div>
         </template>
@@ -13,14 +13,14 @@
       </div>
     </div>
     <!--반복되는 부분-->
-    <div v-for="product in products" :key="product.pid">
+    <div v-for="(product,i) in products" :key="product.pid">
       <div v-for="info in infos" :key="info.pid">
         <template v-if="product.pid===info.pid">
         <v-divider></v-divider>
         <div class="white d-flex ma-0 pa-0 mb-2 mt-5">
           <v-row class="ma-0">
             <v-col cols="1">
-              <v-checkbox></v-checkbox>
+              <v-checkbox color="black" v-model="selected" :value="i"></v-checkbox>
             </v-col>
             <v-col cols="3" class="pa-0 pl-3">
               <div style="width: 80px">
@@ -48,7 +48,7 @@
                 <v-col cols="12" class="pa-0" style="font-size: 14px; color: grey"
                   >수량 : {{ product.pamount }} 개</v-col
                 >
-                <v-col cols="12" class="font-weight-black ma-0 pt-2">{{info.pprice.toLocaleString()}} 원</v-col>
+                <v-col cols="12" class="font-weight-black ma-0 pt-2">{{(info.pprice*product.pamount).toLocaleString()}} 원</v-col>
               </v-row>
             </v-col>
 
@@ -80,6 +80,8 @@ export default {
     return {
       products: null,
       infos: [],
+      selected:[],
+      count:0
     };
   },
   //컴포넌트 메서드터 정의
@@ -91,6 +93,8 @@ export default {
         .then((response) => {
           this.products = response.data;
           this.getProductInfo(this.products);
+          this.count = this.products.length;
+          console.log(this.count);
         })
         .catch((error) => {
           console.log(error);
@@ -110,8 +114,23 @@ export default {
           });
       }
     },
-    get(){
-      console.log(this.info);
+    selectAll(){
+
+    }
+  },
+  computed:{
+    selectedAll:{
+      set(val){
+        this.selected = []
+        if(val) {
+          for(let i=0; i< this.count; i++){
+            this.selected.push(i);
+          }
+        }
+      },
+      get(){
+        return this.selected.length === this.count
+      }
     }
   },
   created() {
