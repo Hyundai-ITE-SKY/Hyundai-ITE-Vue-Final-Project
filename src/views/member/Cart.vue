3763b4d07b7c98d592afe6dedfc1bd1ae9c8e63b
fxdmin <go.jminn@gmail.com>
2021-11-28 12:27:39 +0900

    [SKY-44] refactor: CartItem 컴포넌트 분리
    - 체크박스 구현 다시

--- src/views/member/Cart.vue
+++ src/views/member/Cart.vue
@@ -15,73 +15,41 @@
     <!--반복되는 부분-->
     <div v-for="(product,i) in products" :key="product.pid">
       <div v-for="info in infos" :key="info.pid">
-        <template v-if="product.pid===info.pid">
-        <v-divider></v-divider>
-        <div class="white d-flex ma-0 pa-0 mb-2 mt-5">
-          <v-row class="ma-0">
-            <v-col cols="1">
-              <v-checkbox color="black" v-model="selected" :value="i"></v-checkbox>
-            </v-col>
-            <v-col cols="3" class="pa-0 pl-3">
-              <div style="width: 80px">
-                <v-img :src="info.cimage1" contain />
-              </div>
-            </v-col>
-            <v-col class="ml-2">
-              <v-row>
-                <v-col
-                  cols="9"
-                  class="pa-0 font-weight-black text-truncate"
-                  style="font-size: 15px"
-                >{{info.bname}}
-                </v-col>
-                <v-col cols="1" class="mr-2 pa-0">
-                  <v-icon class="red--text">mdi-cards-heart</v-icon>
-                </v-col>
-                <v-col cols="1" class="pa-0 pr-1"><v-icon>mdi-close</v-icon></v-col>
-                <v-col cols="12" class="pa-0 text-truncate" style="font-size: 15px">
-                  {{ info.pname }}
-                </v-col>
-                <v-col cols="12" class="pa-0" style="font-size: 14px; color: grey"
-                  >옵션 : {{ product.pcolor }}_{{ product.psize }}</v-col
-                >
-                <v-col cols="12" class="pa-0" style="font-size: 14px; color: grey"
-                  >수량 : {{ product.pamount }} 개</v-col
-                >
-                <v-col cols="12" class="font-weight-black ma-0 pt-2">{{(info.pprice*product.pamount).toLocaleString()}} 원</v-col>
-              </v-row>
-            </v-col>
-
-            <v-layout class="justify-center">
-              <v-card-actions class="mb-3 mt-3 pt-0">
-                <v-btn dark class="mr-3" outlined color="black">옵션/수량</v-btn>
-                <v-btn dark to="/order/order" outlined color="black">바로 구매</v-btn>
-              </v-card-actions>
-            </v-layout>
-          </v-row>
-        </div>
+        <template v-if="product.pid === info.pid">
+          <cart-item
+            :bname="info.bname"
+            :cimage1="info.cimage1"
+            :pcolor="product.pcolor"
+            :pname="info.pname"
+            :pprice="info.pprice"
+            :pamount="product.pamount"
+            :psize="product.psize"
+            :selected="false"
+            :checkboxValue="i.toString()"
+          >
+          </cart-item>
         </template>
       </div>
     </div>
-
   </div>
 </template>
 <script>
 import member from "@/apis/member";
 import product from "@/apis/product";
+import CartItem from "./CartItem.vue";
 
 export default {
   //component의 대표 이름(devTools에 나오는 이름)
   name: "Cart",
   //추가하고 싶은 컴포넌트를 등록
-  components: {},
+  components: { CartItem },
   //컴포넌트 데이터 정의
   data() {
     return {
       products: null,
       infos: [],
-      selected:[],
-      count:0
+      count: 0,
+      selected : []
     };
   },
   //컴포넌트 메서드터 정의
@@ -114,24 +82,21 @@ export default {
           });
       }
     },
-    selectAll(){
-
-    }
   },
-  computed:{
-    selectedAll:{
-      set(val){
-        this.selected = []
-        if(val) {
-          for(let i=0; i< this.count; i++){
+  computed: {
+    selectedAll: {
+      set(val) {
+        this.selected = [];
+        if (val) {
+          for (let i = 0; i < this.count; i++) {
             this.selected.push(i);
           }
         }
       },
-      get(){
-        return this.selected.length === this.count
-      }
-    }
+      get() {
+        return this.selected.length === this.count;
+      },
+    },
   },
   created() {
     if (this.$store.getters["login/getUserId"] === "") {
---
+++ src/views/member/CartItem.vue
@@ -0,0 +1,83 @@
+<!--컴포넌트 UI 정의-->
+<template>
+  <div>
+    <div>
+      <v-divider></v-divider>
+      <div class="white d-flex ma-0 pa-0 mb-2 mt-5">
+        <v-row class="ma-0">
+          <v-col cols="1">
+            <v-checkbox color="black" v-model="selected" :value="checkboxValue"></v-checkbox>
+          </v-col>
+          <v-col cols="3" class="pa-0 pl-3">
+            <div style="width: 80px">
+              <v-img :src="cimage1" contain />
+            </div>
+          </v-col>
+          <v-col class="ml-2">
+            <v-row>
+              <v-col cols="9" class="pa-0 font-weight-black text-truncate" style="font-size: 15px"
+                >{{ bname }}
+              </v-col>
+              <v-col cols="1" class="mr-2 pa-0">
+                <div class="ml-auto" @click="state = !state">
+                  <v-icon v-if="!state">mdi-heart-outline</v-icon>
+                  <v-icon v-if="state" style="color: red">mdi-heart</v-icon>
+                </div>
+              </v-col>
+              <v-col cols="1" class="pa-0 pr-1"><v-icon>mdi-close</v-icon></v-col>
+              <v-col cols="12" class="pa-0 text-truncate" style="font-size: 15px">
+                {{ pname }}
+              </v-col>
+              <v-col cols="12" class="pa-0" style="font-size: 14px; color: grey"
+                >옵션 : {{ pcolor }}_{{ psize }}</v-col
+              >
+              <v-col cols="12" class="pa-0" style="font-size: 14px; color: grey"
+                >수량 : {{ pamount }} 개</v-col
+              >
+              <v-col cols="12" class="font-weight-black ma-0 pt-2"
+                >{{ (pprice * pamount).toLocaleString() }} 원</v-col
+              >
+            </v-row>
+          </v-col>
+
+          <v-layout class="justify-center">
+            <v-card-actions class="mb-3 mt-3 pt-0">
+              <v-btn dark class="mr-3" outlined color="black">옵션/수량</v-btn>
+              <v-btn dark to="/order/order" outlined color="black">바로 구매</v-btn>
+            </v-card-actions>
+          </v-layout>
+        </v-row>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  //컴포넌트의 대표 이름(devtools에 나오는 이름)
+  name: "CartItem",
+  //추가하고 싶은 컴포넌트 등록
+  components: {},
+  //컴포넌트 데이터 정의
+  data() {
+    return {
+      state: false,
+    };
+  },
+  //컴포넌트 메서드 정의
+  methods: {},
+  props: {
+    cimage1: String,
+    bname: String,
+    pname: String,
+    pcolor: String,
+    psize: String,
+    pamount: Number,
+    pprice: Number,
+    selected : Boolean,
+    checkboxValue : String
+  },
+};
+</script>
+<!--컴포넌트 스타일 정의-->
+<style scoped></style>
