e2bd83167ca42242bce361394b4ab4a550a479f8
smc5720 <smc5720@gmail.com>
2021-11-30 10:46:25 +0900

    [SKY-38] refactor: 이벤트 UI 추가

--- src/views/home/EventComponent.vue
+++ src/views/home/EventComponent.vue
@@ -1,20 +1,34 @@
 <!-- 컴포넌트 UI 정의 -->
 <template>
-  <v-card shaped outlined class="mb-5" style="margin-top: -6px">
-    <!--<v-system-bar lights-out></v-system-bar>-->
-    <v-carousel cycle :show-arrows="false" hide-delimiter-background delimiter-icon="mdi-minus">
-      <v-carousel-item>
-        <div>
-          <event-banner imgUrl="event1.jpg" :eid="1"></event-banner>
-        </div>
-      </v-carousel-item>
-      <v-carousel-item>
-        <div>
-          <event-banner imgUrl="event2.png" :eid="2"></event-banner>
-        </div>
-      </v-carousel-item>
-    </v-carousel>
-  </v-card>
+  <div style="background-color: black">
+    <v-card shaped>
+      <!--<v-system-bar lights-out></v-system-bar>-->
+      <v-carousel cycle :show-arrows="false" hide-delimiter-background delimiter-icon="mdi-minus">
+        <v-carousel-item>
+          <div>
+            <event-banner imgUrl="event1.jpg" :eid="1"></event-banner>
+          </div>
+        </v-carousel-item>
+        <v-carousel-item>
+          <div>
+            <event-banner imgUrl="event2.png" :eid="2"></event-banner>
+          </div>
+        </v-carousel-item>
+      </v-carousel>
+    </v-card>
+    <div class="pa-3 align-center" style="display: flex; height: 49px; border-bottom: solid 2px #333333">
+      <div style="color: white; font-size: 0.875rem; font-weight: bolder">
+        <span style="color: #F35516">프론트로우 X 아우어베이커리 TEASER</span> 기대평 이벤트
+      </div>
+      <v-icon class="ml-auto" color="white">mdi-chevron-right</v-icon>
+    </div>
+    <div class="pa-3 align-center" style="display: flex; height: 49px;">
+      <div style="color: white; font-size: 0.875rem; font-weight: bolder">
+        <span style="color: yellow">야구 좋아하면 필수!</span> 폴로 & 엠엘비 발매
+      </div>
+      <v-icon class="ml-auto" color="white">mdi-chevron-right</v-icon>
+    </div>
+  </div>
 </template>
 
 <script>
